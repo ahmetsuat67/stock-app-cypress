91ad767f9c70dabb541cb3ca7e356348821ac7eb
ahmetsuat67 <93923638+ahmetsuat67@users.noreply.github.com>
2023-09-02 12:53:52 +0300

    register cypress

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,7 +1,11 @@
 describe('stock register', () => {
   it('register', () => {
     cy.visit('http://localhost:3000/')
-      
+    cy.viewport(1200, 1200)
+    cy.url().should('include', '/')
+    cy.get('[data-test="registerLink"]').contains("Don't you have an account?").click({ force: true })
+    cy.url().should("include", "/register");
+    
   })
 
 })
