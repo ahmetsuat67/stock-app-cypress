0a9cddebd389d108a14e4d3717e1382857ce2a33
ahmetsuat67 <93923638+ahmetsuat67@users.noreply.github.com>
2023-09-02 13:49:27 +0300

    adding login

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -7,20 +7,20 @@ describe('stock register', () => {
     cy.url().should("include", "/register");
     cy.get("[data-test='registerHeader']").contains("Register")
     cy.get("[data-test='userName']")
-      .should("be.visible").type("alberttest")
+      .should("be.visible").type("testalbert")
     cy.get("[data-test='firstName']")
       .should("be.visible").type("albert")
     cy.get("[data-test='lastName']")
       .should("be.visible").type("test")
     cy.get("[data-test='email']")
-      .should("be.visible").type("alberttest@gmail.com")
+      .should("be.visible").type("testalbert@gmail.com")
     cy.get("[data-test='password']")
       .should("be.visible").type("Clarusway1.")
     cy.get("[data-test='sbmtRegister']")
       .should("be.visible").click({ force: true });
     cy.url().should("include", "/stock");
-   
-    
+    cy.get("[data-test='logoutBtn']", { timeout: 20000 }).should("be.visible").click();
+      
   })
 
 })
