e8705ef9348535d18e23583c3f233d19fb7efe89
ahmetsuat67 <93923638+ahmetsuat67@users.noreply.github.com>
2023-09-02 14:25:45 +0300

    firms

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,16 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("login",() => {
+    cy.url().should("include", "/");
+        cy.get("[data-test='loginHeader']").contains("Login").should("be.visible");
+        cy.get("[data-test='emailLogin']")
+            .should("be.visible").type("asptest1@gmaail.com")
+        cy.get("[data-test='passwordLogin']")
+            .should("be.visible").type("Clarusway1.")
+        cy.get("[data-test='sbmtlogin']")
+            .should("be.visible")
+            .click({ force: true });
+        cy.url().should("include", "/stock");
+    });
+    
