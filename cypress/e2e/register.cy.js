ef0f0344f1b50affd87a2361e236d512d1c6e81f
ahmetsuat67 <93923638+ahmetsuat67@users.noreply.github.com>
2023-09-02 13:11:15 +0300

    register test finish

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -5,6 +5,21 @@ describe('stock register', () => {
     cy.url().should('include', '/')
     cy.get('[data-test="registerLink"]').contains("Don't you have an account?").click({ force: true })
     cy.url().should("include", "/register");
+    cy.get("[data-test='registerHeader']").contains("Register")
+    cy.get("[data-test='userName']")
+      .should("be.visible").type("albert test")
+    cy.get("[data-test='firstName']")
+      .should("be.visible").type("albert")
+    cy.get("[data-test='lastName']")
+      .should("be.visible").type("test")
+    cy.get("[data-test='email']")
+      .should("be.visible").type("alberttest@gmaail.com")
+    cy.get("[data-test='password']")
+      .should("be.visible").type("Clarusway1.")
+    cy.get("[data-test='sbmtRegister']")
+      .should("be.visible").click({ force: true });
+    cy.url().should("include", "/stock");
+   
     
   })
 
